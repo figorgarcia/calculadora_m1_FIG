aed7a06680199bca456adc3f573c351715dd030f
figtisolucoes <infra@figti.com.br>
2025-09-17 10:36:51 -0300

    .

--- App.js
+++ App.js
@@ -4,48 +4,93 @@ import { styles } from './Styles';
 
 export default function App() {
 
+  const [primeiroNumero, setPrimeiroNumero] = useState('');
+  const [segundoNumero, setSegundoNumero] = useState('');
+  const [operador, setOperador] = useState(null);
+  const [resultado, setResultado] = useState(null);
+
+  const [controleNumeral, setControleNumeral] = useState(1);
+
+  function handleNumeral(numero){
+    if(controleNumeral===1){
+      setPrimeiroNumero(primeiroNumero  + numero)
+    }else{
+      setSegundoNumero(segundoNumero  + numero)
+    }
+  }
+
+  function handleOperacao(operacao){
+    setOperador(operacao)
+    setControleNumeral(2)
+  }
+
+  function handleClear(){
+    setPrimeiroNumero('')
+    setSegundoNumero('')
+    setOperador(null)
+    setControleNumeral(1)
+  }
+
+  function handleIgual(){
+    switch(operador){
+      case '+':
+          setResultado(Number(primeiroNumero) + Number(segundoNumero));
+          break;
+      case '-':
+          setResultado(Number(primeiroNumero) - Number(segundoNumero));
+          break;
+      case '/':
+          setResultado(Number(primeiroNumero) / Number(segundoNumero));
+          break;
+    }
+    handleClear()
+  }
 
   return (
     <View style={styles.container}>
       
       <View style={styles.telaDeResultado}>
-        <Text style={{fontSize:80}}>1</Text>
-        <Text style={{fontSize:80}}>+</Text>
-        <Text style={{fontSize:80}}>2</Text>
+        <Text style={{fontSize:80}}>{primeiroNumero}</Text>
+        <Text style={{fontSize:80}}>{operador}</Text>
+        <Text style={{fontSize:80}}>{segundoNumero}</Text>
       </View>
 
       <View style={styles.containerNumeral}>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(1)}>
           <Text style={{fontSize:80}}>1</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(2)}>
           <Text style={{fontSize:80}}>2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleNumeral(3)}>
           <Text style={{fontSize:80}}>3</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.containerNumeral}>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral}  onPress={()=> handleOperacao('+')}>
           <Text style={{fontSize:80}}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleOperacao('-')}>
           <Text style={{fontSize:80}}>-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={()=> handleOperacao('/')}>
           <Text style={{fontSize:80}}>/</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.containerNumeral}>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={handleIgual}>
           <Text style={{fontSize:80}}>=</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botaoNumeral}>
+        <TouchableOpacity style={styles.botaoNumeral} onPress={handleClear}>
           <Text style={{fontSize:80}}>C</Text>
         </TouchableOpacity>
       </View>
+
+      <Text style={{fontSize:80}}>
+        {resultado}
+      </Text>
 
 
     </View>
--- Styles.js
+++ Styles.js
@@ -7,21 +7,29 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lightblue',
+    padding: 10
   },
   telaDeResultado: {
     backgroundColor: 'white',
     flexDirection: 'row',
     justifyContent: 'flex-end',
+    width: '100%',
+    borderRadius: 10,
+    paddingHorizontal: 10
   },
   containerNumeral: {
     flexDirection: 'row',
-    marginTop: 20
+    marginTop: 20,
+    width: '100%',
   },
   botaoNumeral: {
-    backgroundColor:'lightgray',
+    backgroundColor: 'lightgray',
     padding: 20,
     borderRadius: 20,
-    marginLeft: 10
+    marginLeft: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
   }
-  
+
 });
